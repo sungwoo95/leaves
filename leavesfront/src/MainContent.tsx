afce26901f1e59b988efbcddc5a5a7d4d9818594
Kang sungwoo <106954289+midnightkang@users.noreply.github.com>
2025-02-19 15:45:23 +0900

    editor init

--- leavesfront/src/MainContent.tsx
+++ leavesfront/src/MainContent.tsx
@@ -1,7 +1,13 @@
 import { Box } from "@mui/material";
+import Editor from "./Editor";
 
 const MainContent: React.FC = () => {
-  return <Box sx={{ height: "100vh", p: 2 }}>Main Content</Box>;
+  return (
+    <Box sx={{ height: "100vh", p: 2 }}>
+      Main Content
+      <Editor/>
+    </Box>
+  )
 };
 
 export default MainContent;
