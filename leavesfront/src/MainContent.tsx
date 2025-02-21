53c16077c43e6445cb507bcf8c85d18fbed0966c
Kang sungwoo <106954289+midnightkang@users.noreply.github.com>
2025-02-21 19:37:02 +0900

    change editor-background color

--- leavesfront/src/MainContent.tsx
+++ leavesfront/src/MainContent.tsx
@@ -6,12 +6,18 @@ const MainContent: React.FC = () => {
   const theme = useTheme();
 
   return (
-    <Box sx={{ height: "100vh", boxSizing: "border-box", display: "flex", flexDirection: "column" }}>
+    <Box
+      sx={{
+        height: "100vh",
+        boxSizing: "border-box",
+        display: "flex",
+        flexDirection: "column",
+      }}>
       <Box sx={{ mb: 2 }}>Title</Box>
       <Paper
         sx={{
           flex: 1,
-          bgcolor: theme.palette.mode === "dark" ? "black" : "white",
+          bgcolor: theme.palette.mode === "dark" ? "#121212" : "white",
         }}>
         <Editor />
       </Paper>
