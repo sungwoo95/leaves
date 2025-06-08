bf5abbc7966505e19a4920366d0842090f73b084
sungwoo95 <95kswoo@gmail.com>
2025-06-08 20:21:02 +0900

    chore: .env에 serviceAccountKey.json파일의 경로 대신 base64인코딩 값 저장. 의도   왜 경로 대신 값을 저장하는가?   - 컨테이너의 경로와 로컬의 경로 차이로 인하여 컨테이너에서 접근 시 에러 발생.   - serviceAccountKey.json은 remote repo에 없어서, github actions에서 이미지 생성 시 문제 발생.   - serviceAccountKey.json을 .env에 직접 저장하는 방식으로 변경.     (github actions에서는 secrets로 접근하여 저장.)   왜 base64로 저장하는가?     - JSON문자열은 특수문자가 많다. 줄바꿈(\n)이나 따옴표, 중괄호, 백슬래시 등 특수문자가 많아질 때 .env 파서가 제대로 읽지 못할 수 있음 구현 사항   1. serviceAccountKey.json인코딩.     ( base64 -i serviceAccountKey.json | tr -d '\n' )   2. .env에 인코딩된 값 저장.   3. firebase.ts에서 디코딩하여 사용.

--- leavesback/src/firebase.ts
+++ leavesback/src/firebase.ts
@@ -1,10 +1,13 @@
 // firebaseAdmin.ts
 import admin from 'firebase-admin';
 
+const decoded = Buffer.from(process.env.SERVICE_ACCOUNT_KEY!, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(
-      require(process.env.SERVICE_ACCOUNT_KEY as string)
+      serviceAccount
     ),
   });
 }
